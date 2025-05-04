59a513f734a0deeb8f46efa1b79fc36f4428a5e2
ZiyadMoustafa <ziyadmoustafa77@gmail.com>
2025-05-05 02:52:15 +0300

    edit

--- models/organizationModel.js
+++ models/organizationModel.js
@@ -18,15 +18,6 @@ const organizationSchema = new mongoose.Schema({
   },
 });
 
-// Index location for geospatial queries
 organizationSchema.index({ location: "2dsphere" });
 
 module.exports = mongoose.model("Organization", organizationSchema);
-
-// Point = نقطة واحدة (مثلاً مقر شركة).
-
-// LineString = خط (مثلاً شارع أو طريق).
-
-// Polygon = مضلع (مثلاً حدود مدينة أو منطقة).
-
-// في مشروعنا، إحنا بنستخدم النوع Point، لأن كل شركة ليها "موقع ثابت" على الخريطة مش منطقة أو خط.
